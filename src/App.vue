25897566b75a1f7fb17d3fb83afb6d0c094c3a1a
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-12-16 15:38:38 +0300

    primevue added

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <img src="./assets/primevue-logo.png" alt="PrimeVue Logo" />
+    <h2>PrimeVue Issue Template</h2>
+    <p>Please create a test case and attach the link of the codesandbox to your github issue report.</p>
+    <Button type="button" label="PrimeVue Button"></Button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
